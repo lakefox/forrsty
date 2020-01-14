656a76afee1cc497d57da4d156c3ba21b45b9196
LakeFox <mason@lakefox.net>
2020-01-14 09:31:23 -0700

    Add files via upload

--- master.js
+++ master.js
@@ -3,8 +3,10 @@
 // /ipod/?id=483718232&seed=vintage vaporwave vhs boy girl#0
 
 let gifQue = [];
+let questions = [];
 
 window.location.hash = parseInt(window.location.hash.slice(1)) || 0
+loadTriva();
 loadGiphy(seed,parseInt(window.location.hash.slice(1)));
 
 document.querySelector("#soundcloud").src = `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/${id}&color=%23c1c5cb&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true`
@@ -32,8 +34,50 @@ function next() {
   gifQue.shift();
   let current = parseInt(window.location.hash.slice(1));
   window.location.hash = current + 1;
+
   if (gifQue.lenght < 10) {
     loadGiphy(seed,current);
     window.location.hash = current + 25;
+    loadTriva();
   }
+}
+
+setInterval(() => {
+  document.querySelector(`#a${cI}`).classList.add("correct");
+  setTimeout(() => {
+    document.querySelector(`#a${cI}`).classList.remove("correct");
+    setTimeout(nextQuestion,500);
+  },20000);
+},60000);
+
+let cI = 0;
+function nextQuestion() {
+  document.querySelector("#question").innerHTML = questions[0].question;
+  questions[0].incorrect_answers.push(questions[0].correct_answer);
+  let as = shuffle(questions[0].incorrect_answers);
+  cI = as.indexOf(questions[0].correct_answer)+1;
+  document.querySelector("#a1").innerHTML = "A. "+as[0];
+  document.querySelector("#a2").innerHTML = "B. "+as[1];
+  document.querySelector("#a3").innerHTML = "C. "+as[2];
+  document.querySelector("#a4").innerHTML = "D. "+as[3];
+  questions.shift();
+}
+
+function loadTriva() {
+  fetch(`https://opentdb.com/api.php?amount=25&type=multiple`).then((res) => {
+    return res.json();
+  }).then((data) => {
+    questions = questions.concat(data.results);
+    nextQuestion();
+  });
+}
+
+function shuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
 }
