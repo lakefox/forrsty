8920a2dc20bb99dc19866333e0acff338850c7c5
LakeFox <mason@lakefox.net>
2020-01-20 19:00:14 -0700

    Add files via upload

--- master.js
+++ master.js
@@ -13,7 +13,7 @@ window.location.hash = parseInt(window.location.hash.slice(1)) || 0
 loadTriva();
 loadGiphy(seed,parseInt(window.location.hash.slice(1)));
 
-if (!mute) {
+if (typeof mute != "undefined") {
     document.querySelector("#soundcloud").src = `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/${id}&color=%23c1c5cb&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true`;
 }
 
